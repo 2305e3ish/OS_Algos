ceb1e38737edaef1971a3cf53816d081a18d87ca
ish64 <153824395+2305e3ish@users.noreply.github.com>
2025-03-18 04:37:43 +0000

    all ipc modified

--- fifos_pipe.c
+++ fifos_pipe.c
@@ -1,39 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
 #define FIFO_NAME "myfifo"
-#define MSG_SIZE 100
+#define BUFFER_SIZE 100
 
 int main() {
-    int fd;
-    char msg[MSG_SIZE];
+    pid_t pid;
+    char write_msg[BUFFER_SIZE], read_msg[BUFFER_SIZE];
 
     if (mkfifo(FIFO_NAME, 0666) == -1) {
         perror("mkfifo");
+        exit(1);
     }
 
-    pid_t pid = fork();
+    pid = fork();
     if (pid < 0) {
         perror("fork");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
-    if (pid > 0) { 
-        fd = open(FIFO_NAME, O_WRONLY);
-        char *write_msg = "Hello from FIFO";
+    if (pid == 0) { // Child Process
+        int fd = open(FIFO_NAME, O_RDONLY);
+        read(fd, read_msg, BUFFER_SIZE);
+        printf("Child process received: %s\n", read_msg);
+        close(fd);
+    } else { // Parent Process
+        int fd = open(FIFO_NAME, O_WRONLY);
+        printf("Enter a message: ");
+        fgets(write_msg, BUFFER_SIZE, stdin);
         write(fd, write_msg, strlen(write_msg) + 1);
         close(fd);
-    } else { 
-        fd = open(FIFO_NAME, O_RDONLY);
-        read(fd, msg, MSG_SIZE);
-        printf("Child read from FIFO: %s\n", msg);
-        close(fd);
+        wait(NULL);
+        unlink(FIFO_NAME);
     }
-
-    unlink(FIFO_NAME);
     return 0;
 }
--- msg_que.c
+++ msg_que.c
@@ -1,47 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 100
 
 struct msg_buffer {
     long msg_type;
-    char msg_text[100];
+    char msg_text[BUFFER_SIZE];
 };
 
 int main() {
-    key_t key;
+    pid_t pid;
     int msgid;
+    key_t key;
+    struct msg_buffer message;
+
     key = ftok("progfile", 65);
+    if (key == -1) {
+        perror("ftok");
+        exit(EXIT_FAILURE);
+    }
 
     msgid = msgget(key, 0666 | IPC_CREAT);
-    if(msgid == -1) {
+    if (msgid == -1) {
         perror("msgget");
         exit(EXIT_FAILURE);
     }
-    
-    pid_t pid = fork();
+
+    pid = fork();
     if (pid < 0) {
         perror("fork");
         exit(EXIT_FAILURE);
     }
-    
-    if (pid > 0) { 
-        struct msg_buffer message;
-        message.msg_type = 1;
-        strcpy(message.msg_text, "Hello from message queue");
-        msgsnd(msgid, &message, sizeof(message.msg_text), 0);
-        printf("Parent: Message sent\n");
-    } else { 
-        struct msg_buffer message;
+
+    if (pid == 0) { // Child Process
         msgrcv(msgid, &message, sizeof(message.msg_text), 1, 0);
-        printf("Child received: %s\n", message.msg_text);
+        printf("Child process received: %s\n", message.msg_text);
+        msgctl(msgid, IPC_RMID, NULL); // Remove the message queue
+    } else { // Parent Process
+        message.msg_type = 1;
+        printf("Enter a message: ");
+        fgets(message.msg_text, BUFFER_SIZE, stdin);
+        msgsnd(msgid, &message, sizeof(message.msg_text), 0);
+        wait(NULL); // Wait for the child process to finish
     }
-    
-    if (pid > 0) {
-        sleep(1);
-        msgctl(msgid, IPC_RMID, NULL);
-    }
+
     return 0;
 }
